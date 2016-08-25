90ad2e8be56c85750f978d9872cef4e3e3577267
Zhongqi Wang <zhongqi@utexas.edu>
2016-08-25 14:13:29 -0500

    Add files via upload

--- blink.c
+++ blink.c
@@ -7,12 +7,10 @@
 
 /* This bit of code below makes this example work on all 96Boards */
 unsigned int GPIO_LED;
-unsigned int GPIO_BUTTON;
 
 __attribute__((constructor)) static void _init()
 {
 	board_config *config = libsoc_board_init();
-	GPIO_BUTTON = libsoc_board_gpio_id(config, "GPIO-E");
 	GPIO_LED = libsoc_board_gpio_id(config, "GPIO-G");
 	libsoc_board_free(config);
 }
@@ -21,30 +19,22 @@ __attribute__((constructor)) static void _init()
 
 int main()
 {
-	gpio *gpio_led, *gpio_button;
+	gpio *gpio_led;
 
 	gpio_led = libsoc_gpio_request(GPIO_LED, LS_SHARED);
-	gpio_button = libsoc_gpio_request(GPIO_BUTTON, LS_SHARED);
-
-	if ((gpio_led == NULL) || (gpio_button == NULL)) {
+    
+	if ((gpio_led == NULL)) {
 		fprintf(stderr, "Unable to request GPIOs\n");
         	goto fail;
 	}
 
 	libsoc_gpio_set_direction(gpio_led, OUTPUT);
-	libsoc_gpio_set_direction(gpio_button, INPUT);
 
-	if ((libsoc_gpio_get_direction(gpio_led) != OUTPUT)
-	     || (libsoc_gpio_get_direction(gpio_button) != INPUT)) {
+	if ((libsoc_gpio_get_direction(gpio_led) != OUTPUT)) {
 		fprintf(stderr, "Unable to set led/btn directions\n");
 		goto fail;
 	}
 	
-	if (libsoc_gpio_set_edge(gpio_button, BOTH) == EXIT_FAILURE) {
-		fprintf(stderr, "Unable to set btn edge\n");
-		goto fail;
-	}
-
 	while (1) {
 		libsoc_gpio_set_level(gpio_led, 1);
 		usleep(500000);
@@ -56,7 +46,5 @@ fail:
 	if (gpio_led)
 		libsoc_gpio_free(gpio_led);
 
-	if (gpio_button)
-		libsoc_gpio_free(gpio_button);
     return EXIT_SUCCESS;
 }
